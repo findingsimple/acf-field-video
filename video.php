969432cac8d87ff3bbbe5038c4c8eadb13b6a469
Jason Conroy <jason@findingsimple.com>
2013-10-08 14:54:23 +1100

    Strip out some of the comments

--- video.php
+++ video.php
@@ -10,12 +10,7 @@ class acf_field_video extends acf_field
 	/*
 	*  __construct
 	*
-	*  Set name / label needed for actions / filters
-	*
-	*  @since	3.6
-	*  @date	23/01/13
 	*/
-	
 	function __construct()
 	{
 		// vars
@@ -47,16 +42,7 @@ class acf_field_video extends acf_field
 	/*
 	*  create_options()
 	*
-	*  Create extra options for your field. This is rendered when editing a field.
-	*  The value of $field['name'] can be used (like bellow) to save extra data to the $field
-	*
-	*  @type	action
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$field	- an array holding all the field's data
 	*/
-	
 	function create_options( $field )
 	{
 		// defaults?
@@ -122,15 +108,7 @@ class acf_field_video extends acf_field
 	/*
 	*  create_field()
 	*
-	*  Create the HTML interface for your field
-	*
-	*  @param	$field - an array holding all the field's data
-	*
-	*  @type	action
-	*  @since	3.6
-	*  @date	23/01/13
 	*/
-	
 	function create_field( $field )
 	{
 
@@ -162,19 +140,7 @@ class acf_field_video extends acf_field
 	/*
 	*  format_value_for_api()
 	*
-	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$value	- the value which was loaded from the database
-	*  @param	$post_id - the $post_id from which the value was loaded
-	*  @param	$field	- the field array holding all the field options
-	*
-	*  @return	$value	- the modified value
 	*/
-
 	function format_value_for_api($value, $post_id, $field)
 	{
 
@@ -203,15 +169,7 @@ class acf_field_video extends acf_field
 	/*
 	*  input_admin_enqueue_scripts()
 	*
-	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
-	*  Use this action to add css + javascript to assist your create_field() action.
-	*
-	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
-	*  @type	action
-	*  @since	3.6
-	*  @date	23/01/13
 	*/
-
 	function input_admin_enqueue_scripts()
 	{
 		
